4f9e860430dc1e1c0435e12eb64ca5583b504255
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2023-12-14 17:53:55 +0400

    Task 1 - cover util functions with tests

---
+++ projects/ui-kit/src/lib/utils/type-coercion.spec.ts
@@ -0,0 +1,30 @@
+import { isNumber, toBooleanProperty } from "./type-coercion"
+
+describe(`isNumber Function`, () => {
+  it(`should treat integer 100 as a number`, () => {
+    expect(isNumber(100)).toBe(true);
+  })
+  it(`should treat string '100' as a number`, () => {
+    expect(isNumber('100')).toBe(true);
+  })
+  it(`should treat string '' as NOT a number`, () => {
+    expect(isNumber('')).toBe(false);
+  })
+  it(`should treat string '100abc' as NOT a number`, () => {
+    expect(isNumber('100abc')).toBe(false);
+  })
+  it(`should treat string {} as NOT a number`, () => {
+    expect(isNumber({})).toBe(false);
+  })
+})
+describe(`toBooleanProperty Function`, () => {
+  it(`should coerce 'false' to boolean false`, () => {
+    expect(toBooleanProperty('false')).toBe(false);
+  })
+  it(`should coerce '' to boolean true`, () => {
+    expect(toBooleanProperty('')).toBe(true);
+  })
+  it(`should coerce null to boolean false`, () => {
+    expect(toBooleanProperty(null)).toBe(false);
+  })
+})
