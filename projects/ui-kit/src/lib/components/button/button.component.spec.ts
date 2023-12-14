31bb3f9f9fed200418e17eeb58d9ac01fee35493
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2023-12-14 18:17:35 +0400

    Task 6 - How to test component inputs

--- projects/ui-kit/src/lib/components/button/button.component.spec.ts
+++ projects/ui-kit/src/lib/components/button/button.component.spec.ts
@@ -1,19 +1,48 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ButtonComponent } from "./button.component";
 import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 describe('ButtonComponent', () => {
   let fixture: ComponentFixture<ButtonComponent>;
   let debugEl: DebugElement;
+  let el: HTMLElement;
   
   beforeEach(() => {
     TestBed.configureTestingModule({declarations: [ButtonComponent]})
     TestBed.createComponent(ButtonComponent);
     fixture = TestBed.createComponent(ButtonComponent);
     debugEl = fixture.debugElement;
+    el = debugEl.nativeElement;
     fixture.detectChanges();
   });
-  it('should test something...', () => {
-    expect(true).toBe(true);
+  describe('Appearance state', () => {
+    it('should have "solid" appearance by default', () => {
+      // expect(el.classes['solid-button']).toBe(true);
+      // expect(el.nativeElement.classList.contains('solid-button')).toBe(true);
+      expect(el.classList).toContain('solid-button');
+    })
+    it('should apply proper CSS classes when appearance changes', () => {
+      debugEl.componentInstance.appearance = 'stroked';
+      fixture.detectChanges();
+      expect(el.classList).toContain('stroked-button');
+  
+      debugEl.componentInstance.appearance = 'solid';
+      fixture.detectChanges();
+      expect(el.classList).toContain('solid-button');
+    })
+  })
+  describe('Loading state', () => {
+    it('should show loader icon in "loading" state', () => {
+      debugEl.componentInstance.loading = true;
+      fixture.detectChanges();
+      let loader = debugEl.query(By.css('.loader'));
+      expect(loader).not.toBeNull();
+  
+      debugEl.componentInstance.loading = false;
+      fixture.detectChanges();
+      loader = debugEl.query(By.css('.loader'));
+      expect(loader).toBeNull();
+    })
   })
 })
